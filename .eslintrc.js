ac4762064b12bd4e0eb06848ff54d320380e104a
Youthink <hufy3651@foxmail.com>
2018-11-06 13:41:21 +0800

    optimize the eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
   },
   // add your custom rules here
   'rules': {
+    'semi': ['error', 'always'],
+    'no-multi-spaces': ['error', { exceptions: { 'ImportDeclaration': true } }],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
