9dd6412166401fd758787363f097ead9b43ecac2
Youthink <hufy3651@foxmail.com>
2019-04-21 18:07:26 +0800

    config apiUrl

---
+++ src/config/apiUrl.js
@@ -0,0 +1,8 @@
+const host = 'http://192.168.1.9:4000';
+
+export default {
+  addHabit: `${host}/api/habit`,
+  checkIn: `${host}/api/checkIn`,
+  day: `${host}/api/day`,
+  getHabitList: `${host}/api/habit`
+};
